8854fcf8f5df6c853fe944f178bf2ec0b6f70917
daniellawhb <daniellawahab!@gmail.com>
2023-10-02 18:11:56 +0300

    new

--- recipe-project-front/src/pages/layout.js
+++ recipe-project-front/src/pages/layout.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar'; // Import your Navbar component
-import Footer from './Footer';
+
 import { Outlet } from 'react-router-dom' ;
 
 const Layout = () => {
   return (
     <div>
       <header>
-        <Navbar />
+     <Navbar />
       </header>
       <main>
       <Outlet /> {/* Render the content of routed components */}
       </main>
-      <footer>
-        {/* Footer content goes here */}
-      </footer>
+    
     </div>
   );
 }
