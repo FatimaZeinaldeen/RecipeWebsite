ab79f487c5f0b8d9000ab7cbbe3ab37a31b3f06a
zainabb14 <za6597718@gmail.com>
2023-10-04 16:45:23 +0300

    countries

---
+++ recipe-project-front/src/Components/CountryDropDown/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./dropdown.module.css";
+function Countries() {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleDropdownClick = () => {
+    setIsClicked(true);
+  };
+  const countryContext = require.context(
+    "../../assets/Images/",
+    false,
+    /\.(png)$/ // Specify the file extension you want to include
+  );
+  const countryArray=countryContext.keys();
+  const countryNames=countryArray.map((country)=>{
+    return(
+        country.split("/")[1].split(".")[0].split("-")[1]
+    )
+  })
+  return (
+    <div>
+      <form>
+        <select
+          id="select"
+          name="Country"
+          title="Country"
+          className={`${styles.dropdown} ${isClicked ? styles.clicked : ""}`}
+          onClick={handleDropdownClick}
+        >
+          <option className={styles.options} value="Category" hidden>
+            Country
+          </option>
+          {countryNames.map((country) => (
+        <option className={styles.options} value={country}>
+            <p>{country}</p>
+        </option>
+    ))}
+        </select>
+      </form>
+    </div>
+  );
+}
+
+export default Countries;
