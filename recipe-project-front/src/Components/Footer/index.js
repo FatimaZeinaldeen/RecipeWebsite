5199081fefffe69dfb326c0d94ae1521c8ded1bd
daniellawhb <daniellawahab!@gmail.com>
2023-10-05 22:00:40 +0300

    a

--- recipe-project-front/src/Components/Footer/index.js
+++ recipe-project-front/src/Components/Footer/index.js
@@ -7,8 +7,9 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerLeft}>
-        <span className={styles.websiteName}>A taste of home in every Dish
+        <span className={styles.websiteName}>A taste of home in every
         </span>
+        {/* You can add a logo here if desired */}
       </div>
       <div className={styles.footermiddle}>
       <ul className={styles.footlist}>
