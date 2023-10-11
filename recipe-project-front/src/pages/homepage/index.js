c7a9a48cbd6a1abe6fe8a1ce995496016189cfcc
zainabb14 <za6597718@gmail.com>
2023-10-11 20:58:56 +0300

    nn

--- recipe-project-front/src/pages/homepage/index.js
+++ recipe-project-front/src/pages/homepage/index.js
@@ -97,7 +97,7 @@ function Home() {
     setIsClicked(true);    
   }
   const countryContext = require.context(
-    "../../assets/images/",
+    "../../assets/Images/",
     false,
     /\.(png)$/ // Specify the file extension you want to include
   );
