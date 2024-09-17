92a00d448c010e2055fc567d7b33f851786c0dac
FatimaZeinaldeen <144237009+FatimaZeinaldeen@users.noreply.github.com>
2024-09-17 14:27:31 +0300

    Update connectDB.js

--- mongodb-mongoose/config/connectDB.js
+++ mongodb-mongoose/config/connectDB.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const connectdb = async ('mongodb+srv://FatimaZeinaldeen:<FatimaKassemZeinaldeen>@cluster0.pxrudjw.mongodb.net/RecipeDB') => {
+const connectdb = async ('mongodb+srv://FatimaZeinaldeen:FatimaKassemZeinaldeen@cluster0.pxrudjw.mongodb.net/RecipeDB') => {
   try {
-    await mongoose.connect('mongodb+srv://FatimaZeinaldeen:<FatimaKassemZeinaldeen>@cluster0.pxrudjw.mongodb.net/RecipeDB').then(
+    await mongoose.connect('mongodb+srv://FatimaZeinaldeen:FatimaKassemZeinaldeen@cluster0.pxrudjw.mongodb.net/RecipeDB').then(
       () => console.log("connected successfully..."),
       (err) => console.log("connection error", err)
     );
